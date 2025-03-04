2a6e46c10a0220de0d4e835f58e9c5439fefe5ba
blackcodersec <72689232+blackcodersec@users.noreply.github.com>
2025-03-04 22:43:34 +0600

    Update subprober.py

--- subprober/subprober.py
+++ subprober/subprober.py
@@ -1,9 +1,15 @@
+import sys
+import os
+
+# Add the parent directory to the Python path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(script_dir)
+sys.path.insert(0, parent_dir)
+
+# Now use a relative import
 from subprober.modules.handler import Main
 
 def main():
-    
-   Main()
-    
+    Main()
 if __name__ == '__main__':
-    
     main()
